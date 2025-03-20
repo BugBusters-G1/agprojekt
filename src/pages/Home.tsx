e21471f14946f711eaabf32356af9890d46abd35
dkslinkyyy <slinky.sthlm@gmail.com>
2025-03-20 13:51:29 +0100

    Add: React-router-dom install and configuration

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Card } from "../components/Card/Card";
+import { useJokes } from "../hooks/useJokes";
+import "../App.css";
+
+const Home = () => {
+  const { loading, currentJoke, getRandomJoke } = useJokes();
+
+  if (loading) {
+    return <div>Loading...</div>; // Show loading message or spinner while waiting for data
+  }
+
+  return (
+    <main>
+      {currentJoke ? (
+        <>
+          <Card joke={currentJoke} onGenerateNewJoke={getRandomJoke} />
+        </>
+      ) : (
+        <div>Inga skämt tillgängliga.</div>
+      )}
+    </main> //getRandomJoke kommer sedan kallas på via komponenten button som vi ska skapa
+  );
+};
+
+export default Home;
