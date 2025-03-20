f9d12831862381f2583b36554b7c02c415240bc1
amandalyckenius <alyckenius@gmail.com>
2025-03-20 13:51:44 +0100

    Add: Header component and Hamburger component files

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import Home from "./pages/Home";
 
 function App() {
   return (
+<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
+=======
+
+    <main>
+      {currentJoke ? (
+        <>
+          <Card joke={currentJoke} onGenerateNewJoke={getRandomJoke} />
+        </>
+      ) : (
+        <div>Inga skämt tillgängliga.</div>
+      )}
+    </main> //getRandomJoke kommer sedan kallas på via komponenten button som vi ska skapa
+
+  
+>>>>>>> 3f9f466 (Add: Header component and Hamburger component files)
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { HamburgerMenu } from "./HamburgerMenu";
+
+export function Header() {
+    return (
+        <header>
+            <p>Här ska loggan ligga som en img. 
+                Vi kan skapa en länk runt bilden som länkar tillbaka till startsidan</p>
+
+        
+            <HamburgerMenu/>
+
+        </header>
+    )
+}
