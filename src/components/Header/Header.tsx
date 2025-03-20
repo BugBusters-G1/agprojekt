5fb7069b55fee372c28406001825ab2c82ecf58e
amandalyckenius <alyckenius@gmail.com>
2025-03-20 14:23:13 +0100

    Change: text in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,7 @@ import { HamburgerMenu } from "./HamburgerMenu";
 export function Header() {
     return (
         <header>
-            <p>Här ska loggan ligga som en img. 
-                Vi kan skapa en länk runt bilden som länkar tillbaka till startsidan</p>
+            <p>Lagom kul</p>                      {/*Här ska loggan ligga som en img.  Vi kan skapa en länk runt bilden som länkar tillbaka till startsidan */}
 
         
             <HamburgerMenu/>
