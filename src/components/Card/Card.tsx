2ee0abde32c9fac0a93de8ed837696bb359f10fb
amandalyckenius <alyckenius@gmail.com>
2025-03-20 11:13:06 +0100

    Add: CSS to Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,29 @@
 import { Joke } from "../../types/Joke";
+import "./Card.css"
 
 export function Card({
   category,
   swedish,
   direct_translation,
   meaning,
+
+
 }: Joke) {
 
    
 
   return (
-    <div>
-      <div>
-        <p>Test</p>
+    <div className="card-box">
+      <div className="card-content">
+        
         <p>{swedish}</p>
         <p>{direct_translation}</p>
+
+
+
       </div>
+     
+
     </div>
   );
 }
