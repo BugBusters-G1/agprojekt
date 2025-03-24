284411238ab314ee6da95bb4b063e39620ff1404
dkslinkyyy <slinky.sthlm@gmail.com>
2025-03-24 13:48:50 +0100

    Fix: Merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Navbar } from "./components/Navbar/Navbar";
-import FilterView from "./pages/FilterView";
-import SavedJokesView from "./pages/SavedJokesView";
 import { useJokes } from "./hooks/useJokes";
+import { Filter } from "./components/Filter/Filter";
+import { useState } from "react";
 
 function App() {
-
-
   const { loading, error, currentJoke, getRandomJoke } = useJokes();
-
+  const [isFilterMenu, setFilterMenu] = useState<boolean>(false);
 
   return (
     <>
+      <BrowserRouter>
+        <Navbar onGenerateNewJoke={getRandomJoke} />
 
-    
-    <BrowserRouter>
-      <Navbar onGenerateNewJoke= {getRandomJoke}/>
-      <Routes>
-        <Route path="/" element={
-          <Home 
-              loading={loading}
-              currentJoke={currentJoke}
-              error={error}
-              getRandomJoke={getRandomJoke}/>}
-           />
-        <Route path="/filter" element={<FilterView/>} />
-        <Route path="/saved" element={<SavedJokesView/>} />
-      </Routes>
-    </BrowserRouter>
+        {isFilterMenu && <Filter />}
 
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                loading={loading}
+                currentJoke={currentJoke}
+                error={error}
+                getRandomJoke={getRandomJoke}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,7 @@
+import "./Flter.css"
+
+export function Filter() {
+    return <div className="filter-view">
+
+    </div>
+}
