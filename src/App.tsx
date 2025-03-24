655b1c97fd175590700882f927f8647a5f31ae37
46729 <shanivari@yahoo.com>
2025-03-24 11:13:27 +0100

    Add: simple test view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Filter } from "./components/Filter/Filter";
 
 function App() {
   return (
     <>
-      <Navbar />
       <BrowserRouter>
+
+      <Navbar />
+      <Filter/>
+
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
