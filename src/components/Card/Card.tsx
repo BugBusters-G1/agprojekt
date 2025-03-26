35aa01303aa0bf4860345d2bb59bd5e3374919da
Floremila <flormequi@gmail.com>
2025-03-26 15:21:20 +0100

    Update: card.tsx and colors.tsx adding colorLine.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,26 @@
 import "./Card.css";
 import { Joke } from "../../types/Joke";
+import { categoryColors } from "../../utils/Colors";
 
 interface CardProps {
   joke: Joke;
   expanded: boolean;
 }
 
-const categoryStyles: Record<string, { background: string; text: string }> = {
-  sarcastic_humor: { background: "#FFCC0B", text: "#000000" },
-  wordplay: { background: "#FF6B00", text: "#000000" },
-  klassiker: { background: "#235AFD", text: "#FFFFFF" },
-  default: { background: "#FFCC0B", text: "#000000" },
-};
+
 
 export function Card({ joke, expanded }: CardProps) {
   const style =
-    categoryStyles[joke.categoryInEnglish] || categoryStyles.default;
-
+    categoryColors[joke.category.toLowerCase()] || categoryColors.default;
   return (
-    <div className={`card-box`}>
+    <div className={`card-box`} style={{backgroundColor: style.background, color: style.text}}>
       <div className="card-content">
         <p className="category">{joke.categoryInSwedish}</p>
         <p className="swedish">{joke.jokeInSwedish}</p>
+
       </div>
       {expanded && (
-        <div className="card-content expanded">
+        <div className="card-content expanded" style={{borderTop: `1px solid ${style.lineColor}`}}>
           <p className="translation">{joke.jokeInEnglish}</p>
           <p className="meaning">{joke.meaningEnglish}</p>
           <div className="category-container">
