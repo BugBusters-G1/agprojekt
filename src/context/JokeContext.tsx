5e84cd7d57db45f638be321aa5c2531b0d024d13
dkslinkyyy <slinky.sthlm@gmail.com>
2025-03-31 12:58:36 +0200

    Add: JokeContext

--- src/context/JokeContext.tsx
+++ src/context/JokeContext.tsx
@@ -1,1 +1,65 @@
-export const JokeContext = () => {};
+import { createContext, ReactNode, useContext, useState } from "react";
+import { useJokes } from "../hooks/useJokes";
+import { useCategories } from "../hooks/useCategories";
+import { Joke } from "../types/Joke";
+import { useCopyJoke } from "../hooks/useCopyJoke";
+
+interface JokesContextType {
+  loading: boolean;
+  error: string | null;
+  currentJoke: Joke | null;
+  getRandomJoke: () => void;
+  selectedCategories: string[];
+  updateSelectedCategories: (category: string) => void;
+  categoryError: string | null;
+  categoryLoading: boolean;
+  copyJokeToClipboard: (joke: Joke, expanded: boolean) => void;
+}
+
+const JokesContext = createContext<JokesContextType | null>(null);
+
+export const JokesProvider = ({ children }: { children: ReactNode }) => {
+  const { loading, error, currentJoke, getRandomJoke } = useJokes();
+  const { copyJokeToClipboard } = useCopyJoke();
+  const {
+    categories,
+    error: categoryError,
+    loading: categoryLoading,
+  } = useCategories();
+
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const updateSelectedCategories = (category: string) => {
+    setSelectedCategories((prev) =>
+      prev.includes(category)
+        ? prev.filter((c) => c !== category)
+        : [...prev, category]
+    );
+  };
+
+  return (
+    <JokesContext.Provider
+      value={{
+        loading,
+        error,
+        currentJoke,
+        getRandomJoke: () => getRandomJoke(selectedCategories),
+        selectedCategories,
+        updateSelectedCategories,
+        categoryError,
+        categoryLoading,
+        copyJokeToClipboard,
+      }}
+    >
+      {children}
+    </JokesContext.Provider>
+  );
+};
+
+export const useJokesContext = () => {
+  const context = useContext(JokesContext);
+  if (!context) {
+    throw new Error("useJokesContext must be used within a JokesProvider");
+  }
+  return context;
+};
