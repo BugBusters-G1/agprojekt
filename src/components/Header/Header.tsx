b8cc08f099852252dd3b43c113c5125646a70d20
amandalyckenius <alyckenius@gmail.com>
2025-04-08 10:28:20 +0200

    Add: reset function for UI and jokequeue when click on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,21 @@ import LogoWithName from "../../assets/LogoWithName.png";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { useAppContext } from "../../context/AppContext";
+import { useJokesContext } from "../../context/JokeContext";
 
 export function Header() {
-  const { isCardExpanded } = useAppContext();
+  const { isCardExpanded, resetUI } = useAppContext();
+  const { resetJokes } = useJokesContext();
+
+
+  const resetApp = () => {
+    resetUI();
+    resetJokes();
+  }
+
   return (
     <header
+      onClick={resetApp}
       className={"app-header"}
       style={{
         backgroundColor: isCardExpanded ? "#fffcf7" : "transparent",
