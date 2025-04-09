18e396ef3054d016ec125a8dbb29758989f4c80e
dkslinkyyy <slinky.sthlm@gmail.com>
2025-04-09 15:53:10 +0200

    Fix: buggs from merge with SCRUM-130

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,17 +13,17 @@ interface CardProps {
   expanded: boolean;
   index: number;
 }
-
 export function Card({ joke, expanded, index }: CardProps) {
   const style =
     categoryColors[joke.category.toLowerCase()] || categoryColors.default;
   const { toggleCardExpand } = useAppContext();
 
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
   return (
     <div
-      className={`rounded-xl select-none h-auto w-80 p-4 z- ${
-        index !== 1 ? "shadow-xl" : "shadow-2xl"
-      }`}
+      className={`rounded-[15px] select-none h-auto lg:min-h-150 lg:max-h-150 w-80 flex flex-col gap-2 lg:w-200 p-3 ${
+        isDesktop ? "flex flex-row " : "block"
+      }${index != 1 ? "shadow-lg" : "shadow-2xl"}`}
       style={{
         backgroundColor: style.background,
         color: style.text,
--- src/components/Card/CardBtn.tsx
+++ src/components/Card/CardBtn.tsx
@@ -18,18 +18,14 @@ export function CardButton({
   textColor = "#000",
   className = "",
 }: CardButtonProps) {
-
   return (
     <button
-      onMouseDown={() => setIsPressed(true)}
-      onMouseUp={() => setIsPressed(false)}
-      onMouseLeave={() => setIsPressed(false)}
       onClick={onClick}
       style={{
         backgroundColor: "#ffffff",
         color: textColor,
       }}
-      className={`flex items-center justify-center rounded-full px-3 py-[3px] text-base font-medium transition-colors duration-150 shadow-[0_4px_8px_rgba(0,0,0,0.30)] cursor-pointer ${className}`}
+      className={`flex items-center justify-center rounded-full px-3 py-[3px] text-base font-medium transition-colors duration-150 shadow-[0_4px_8px_rgba(0,0,0,0.30)]`}
     >
       <span className="flex items-center gap-2">
         <span>{label}</span>
