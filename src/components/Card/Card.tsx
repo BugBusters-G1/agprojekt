74858e728a4339b27ad33385fc647e6eddbf303d
amandalyckenius <alyckenius@gmail.com>
2025-04-09 11:19:48 +0200

    Cleanup: Removed dev certs and copy logic from Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,4 @@
-import { useRef, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
-import { useCopyJoke } from "../../hooks/useCopyJoke";
 import { Joke } from "../../types/Joke";
 import { categoryColors } from "../../utils/Colors";
 import { CardButton } from "./CardBtn";
@@ -16,47 +14,12 @@ export function Card({ joke, expanded, index }: CardProps) {
   const style =
     categoryColors[joke.category.toLowerCase()] || categoryColors.default;
   const { toggleCardExpand } = useAppContext();
-  const {copyJokeToClipboard} = useCopyJoke();
 
-  const timerRef = useRef<number| null>(null);
-  const [longPressTriggered, setLongPressTriggered] = useState(false)
-
-  const handlePressStart = (e: React.TouchEvent | React.MouseEvent) => {
-
-    if ("button" in e) { return }
-
-    setLongPressTriggered(false)
-    timerRef.current = window.setTimeout(() => {
-      copyJokeToClipboard(joke, expanded)
-      setLongPressTriggered(true)
-    }, 900)
-  }
-
-  const handlePressEnd = () => {
-    if(timerRef.current) {
-      clearTimeout(timerRef.current)
-    }
-  }
-
-  const handleClick = () => {
-    if (!longPressTriggered) {
-      toggleCardExpand();
-    }
-  }
-
-  const color = "#235AFD";
   return (
     <div
-      onMouseDown={(e) => handlePressStart(e)}
-      onMouseUp={handlePressEnd}
-      onMouseLeave={handlePressEnd}
-      onTouchStart={(e) => handlePressStart(e)}
-      onTouchEnd={handlePressEnd}
-      onClick={() => {
-        handleClick()
-      }}
+      onClick={toggleCardExpand}
       className={`rounded-xl select-none h-auto w-80 p-4 z- ${
-        index != 1 ? "shadow-xl" : "shadow-2xl"
+        index !== 1 ? "shadow-xl" : "shadow-2xl"
       }`}
       style={{
         backgroundColor: style.background,
